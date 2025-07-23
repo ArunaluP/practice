50a9f51ac08800a3cb3eeb7679882339b3696602
ArunaluP <157888762+ArunaluP@users.noreply.github.com>
2025-07-23 16:13:43 +0530

    take Input in Java script

--- index.js
+++ index.js
@@ -4,10 +4,22 @@
 //document.getElementById("firstOne").textContent= "hello world";
 //document.getElementById("first").textContent = "This is my first paragraph in javascript course";
 
-let fullName = "Arunalu Prabhatha";
-let myGpa = 3.009;
-let isStudent = true;
+// let fullName = "Arunalu Prabhatha";
+// let myGpa = 3.009;
+// let isStudent = true;
 
-document.getElementById("num1").textContent = `My name is ${fullName}`;
-document.getElementById(`num2`).textContent = `My GPA is ${myGpa}`;
-document.getElementById(`num3`).textContent = `Is student: ${isStudent}`
+// document.getElementById("num1").textContent = `My name is ${fullName}`;
+// document.getElementById(`num2`).textContent = `My GPA is ${myGpa}`;
+// document.getElementById(`num3`).textContent = `Is student: ${isStudent}`
+
+// let username;
+// username = prompt("Enter your name");
+
+// console.log(`Hello ${username}, welcome to the JavaScript course!`);
+
+let user;
+
+document.getElementById("sub").onclick = function() {
+    user = document.getElementById("name").value;
+    document.getElementById("myh1").textContent = `Hello ${user}`;
+}
