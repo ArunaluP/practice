0f354ae2a12ffae0ab7efa7dcb2399319e77facc
ArunaluP <157888762+ArunaluP@users.noreply.github.com>
2025-07-23 20:36:17 +0530

    Type Conversion constants'

--- index.js
+++ index.js
@@ -17,9 +17,24 @@
 
 // console.log(`Hello ${username}, welcome to the JavaScript course!`);
 
-let user;
+// let user;
 
+// document.getElementById("sub").onclick = function() {
+//     user = document.getElementById("name").value;
+//     document.getElementById("myh1").textContent = `Hello ${user}`;
+// }
+// let input = window.prompt("Enter your text");
+// input = Number(input);
+// input = String(input)
+// input = Boolean(input);
+// console.log(input , typeof input);
+
+const PI = 3.142857142857143;
+let radius;
+let circumference;
+ 
 document.getElementById("sub").onclick = function() {
-    user = document.getElementById("name").value;
-    document.getElementById("myh1").textContent = `Hello ${user}`;
+    radius = document.getElementById("radius").value;
+    circumference = 2 * PI * radius;
+    document.getElementById("myh3").textContent = `Circumference is ${circumference}`;
 }
