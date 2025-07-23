547a3a75526414a3db81c844692bb282241e4ea9
ArunaluP <157888762+ArunaluP@users.noreply.github.com>
2025-07-23 12:51:44 +0530

    variables

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-window.alert(`You are Hacked`)
-console.log(`Hey leave the page you are now hacked version 1`)
+//window.alert(`You are Hacked`)
+//console.log(`Hey leave the page you are now hacked version 1`)
 
-document.getElementById("first").textContent= "hello world";
-document.getElementById("First-paragraph").textContent = "This is my first paragraph in javascript course";
+//document.getElementById("firstOne").textContent= "hello world";
+//document.getElementById("first").textContent = "This is my first paragraph in javascript course";
+
+let fullName = "Arunalu Prabhatha";
+let myGpa = 3.009;
+let isStudent = true;
+
+document.getElementById("num1").textContent = `My name is ${fullName}`;
+document.getElementById(`num2`).textContent = `My GPA is ${myGpa}`;
+document.getElementById(`num3`).textContent = `Is student: ${isStudent}`
