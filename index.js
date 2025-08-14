a74d9f256a025fe0e9fbe21333c9fe07107237d3
ArunaluP <157888762+ArunaluP@users.noreply.github.com>
2025-08-14 07:43:31 +0530

    after a long time

--- index.js
+++ index.js
@@ -1,18 +1,8 @@
-// setTimeout(function(){
-//     console.log("I am the hero")
-// },3000)
+const person = { 
+    name : `Arunalu Prabhatha`,
+    age : 23,
+    school : `Rajapaksha Central College`,
+    increment : () => console.log(`Incremented age: ${++person.age}`),
+}
 
-// const sum = (a,b) =>{ a=a+b;return a+b}
-
-
-// console.log(sum(1,2))
-
-const numbers = [1,2,3,4,5]
-
-const square = numbers.map((element) => Math.pow(element,2))
-const cube = numbers.map((element) => Math.pow(element,3))
-const isEven = numbers.filter((element) => element%2==0)
-const isOdd = numbers.filter((element) => element%2!=0)
-const sum = numbers.reduce((a,b) => a+b)
-
-console.log(sum)
+person.increment()
