ccadeee129829facf8579a262178a1feacf8341d
ArunaluP <157888762+ArunaluP@users.noreply.github.com>
2025-08-18 10:15:32 +0530

    after ELEment selectors

--- index.js
+++ index.js
@@ -1,43 +1,42 @@
-const display = document.getElementById("stopwatch")
-const startbtn = document.getElementById("start")
-const stopbtn = document.getElementById("stop")
-const resetbtn = document.getElementById("reset")
+// Element Selectors
 
-let timer = null;
-let runtime = 0;
-let startTime = 0;
-let isrunning = false;
+// document.getElementById  ELEMENT OR NULL
+// document.getElementByClassName  HTML COLLECTION
+// document.getElementsByTagName  HTML COLLECTION
+// document.querySelector  FIRSTELEMENT OR NULL
+// document.querySelectorAll  NODELIST
 
-startbtn.onclick =  function(){
-    if(!isrunning){
-        startTime = Date.now() - runtime;
-        timer  = setInterval(updateTime,10)
-        isrunning = true;
-    }
-}
+// const first = document.getElementById('first');
+// console.log(first);
+// first.style.fontFamily = 'Arial';
+// first.style.fontSize = '20px';
+// first.style.fontWeight = 'bold';
+// first.style.marginBottom = '10px';
+// first.textContent = `THIS my first javascript Course`
 
-stopbtn.onclick = function(){
-    if(isrunning){
-        clearInterval(timer);
-        isrunning = false;
-    }
-}
-resetbtn.onclick = function(){
-    if(isrunning){
-        clearInterval(timer);
-        isrunning = false;
-    }
-    runtime = 0;
-    display.textContent = "00:00:00:00";}
+//const fruits = document.getElementsByClassName(`fruits`)
 
-function updateTime(){
-    runtime = Date.now() - startTime;
-    const hours  = Math.floor(runtime/ (1000*60*60)).toString().padStart(2, '0');
-    const minutes  = Math.floor(runtime/ (1000*60) % 60).toString().padStart(2, '0');
-    const seconds  = Math.floor(runtime/ (1000) % 60).toString().padStart(2, '0');
-    const miliseconds  = Math.floor(runtime/ (10) % 100).toString().padStart(2, '0');
+//fruits[0].style.backgroundColor = 'lightgreen'; 
 
-   display.textContent = `${hours}:${minutes}:${seconds}:${miliseconds}`;
-}
+// for(let fruit of fruits){
+//     fruit.style.backgroundColor = 'lightgreen';
+// }
+//fruits.forEach(fruit => {fruit.style.backgroundColor = 'lightgreen';}) // this will not change the background color because 
+//HTML collections does not have forEach method
+//Array.from(fruits).forEach(fruit => {fruit.style.backgroundColor = 'lightgreen';})
+const lis = document.getElementsByTagName('li');
+//console.log(lis);
+//Array.from(lis).forEach(li => {li.style.color = 'blue';})
 
+// const fruit = document.querySelector('.fruits')
 
+// fruit.style.backgroundColor = 'lightgreen';
+// const vegetable = document.querySelector('li')
+// vegetable.style.backgroundColor = 'lightgreen';
+const fruits = document.querySelectorAll('.fruits');
+const vegetables = document.querySelectorAll('li');
+
+console.log(fruits);
+vegetables.forEach(vegetable => vegetable.style.backgroundColor = 'blue');
+
+fruits.forEach(fruit => fruit.style.backgroundColor = 'lightgreen');
